2629219c418a04a9a4e038b4aeb436b8b1a7fa63
SchroSct <clls760@gmail.com>
2016-10-05 20:04:00 -0700

    Update cambot-start.bash

--- cambot-start.bash
+++ cambot-start.bash
@@ -4,19 +4,19 @@ then
   mapfile -t models < <(curl -s https://domain.tld/sm-models.txt)
   for m in "${models[@]}"
   do
-    cambot-monitor.sh "${m}" "sm"
+    cambot-monitor.bash "${m}" "sm"
   done
   unset models
   mapfile -t models < <(curl -s https://domain.tld/mfc-models.txt)
   for m in "${models[@]}"
   do
-    cambot-monitor.sh "${m}" "mfc"
+    cambot-monitor.bash "${m}" "mfc"
   done
   unset models
   mapfile -t models < <(curl -s https://domain.tld/cb-models.txt)
   for m in "${models[@]}"
   do
-    cambot-monitor.sh "${m}" "cb"
+    cambot-monitor.bash "${m}" "cb"
   done
   unset models
   rmdir /tmp/cambot.lock/
